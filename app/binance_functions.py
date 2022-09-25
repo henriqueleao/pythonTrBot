9c871bf9daaaaf72a80b74a83c5ae4c1532149a5
Henrique Leão <henrique.saraiva@gmail.com>
2022-09-25 19:11:12 -0300

    testes binance

--- app/binance_functions.py
+++ app/binance_functions.py
@@ -1,6 +1,8 @@
 import os
 import json
 from binance.client import Client
+from decimal import *
+from binance.helpers import round_step_size
 
 class BinanceUtil:
 
@@ -9,10 +11,68 @@ class BinanceUtil:
         self.secretKey = secretKey
         self.client = Client(self.apiKey, self.secretKey, testnet=testnet)
         self.client.API_URL = 'https://testnet.binance.vision/api'
+        self.percentualSizeTrade = 10
         self.concurrentTrades = 5
 
+    def get_account_balance(self, asset):
+        balances = self.client.futures_account_balance()
+        for balance in balances:
+            if balance['asset'] == asset:
+                result = balance['balance']
+        return float(result)
 
+    def get_step_size(self, symbol):
+        info = self.client.futures_exchange_info()
+        for item in info['symbols']:
+            if item['symbol'] == symbol:
+                for f in item['filters']:
+                    if f['filterType'] == 'LOT_SIZE':
+                        return f['stepSize']
 
+    def get_tick_size(self, symbol):
+        info = self.client.futures_exchange_info()
+        for item in info['symbols']:
+            if item['symbol'] == symbol:
+                for f in item['filters']:
+                    if f['filterType'] == 'PRICE_FILTER':
+                        return f['tickSize']
 
+    def get_list_trades(self):
+        futures_account = self.client.futures_account()
+        openedPositions = []
+        for position in futures_account['positions']:
+            if position['entryPrice'] != '0.0':
+                openedPositions.append(position)
+        return (openedPositions)
 
+    def openLong(self, symbol, leverage, stopLossPrice, TakeProfitPrice):
+        balance = self.get_account_balance(asset='USDT') * self.percentualSizeTrade / 100
+        self.client.futures_change_leverage(symbol=symbol, leverage=leverage)
+        tick_size = float(self.get_tick_size(symbol))
+        step_size = float(self.get_step_size(symbol))
+        symbol_info = self.client.get_ticker(symbol=symbol)
+        symbol_price = float(symbol_info['lastPrice'])
+        qty_bruta = Decimal(balance / symbol_price)
+        quantity = round_step_size(qty_bruta, step_size)
+        market_order_long = self.client.futures_create_order(
+            symbol=symbol,
+            side='BUY',
+            type='MARKET',
+            quantity=quantity
+        )
 
+        sell_gain_market_long = self.client.futures_create_order(
+            symbol=symbol,
+            side='SELL',
+            type='TAKE_PROFIT_MARKET',
+            quantity=quantity,
+            stopPrice=take_profit_price
+        )
+
+        sell_stop_market_short = self.client.futures_create_order(
+            symbol=symbol,
+            side='SELL',
+            type='STOP_MARKET',
+            quantity=quantity,
+            stopPrice=stop_loss_price
+        )
